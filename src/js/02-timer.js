ad41fc367c7ee7d3b31ead3ccd06a2bec6dcb6d1
IvanKostishin <112808887+IvanKostishin@users.noreply.github.com>
2023-02-26 18:13:38 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,64 +2,75 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const dataTime = document.querySelector('input#datetime-picker');
-const startButton = document.querySelector('[data-start]');
-const daysValue = document.querySelector('span[data-days]');
-const hoursValue = document.querySelector('span[data-hours]');
-const minutesValue = document.querySelector('span[data-minutes]');
-const secondsValue = document.querySelector('span[data-seconds]');
-startButton.disabled = true;
-let selectedDates = null;
+
+const startBtn = document.querySelector('button[data-start]');
+const dateChosen = document.querySelector('#datetime-picker');
+const d = document.querySelector('[data-days]');
+const h = document.querySelector('[data-hours]');
+const m = document.querySelector('[data-minutes]');
+const s = document.querySelector('[data-seconds]');
+
+let timer = null;
+
+startBtn.disabled = true;
+
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selected) {
-    startButton.disabled = false;
-    selectedDates = selected;
+  onClose(selectedDate) {
+    if (selectedDate[0] <= new Date()) {
+      startBtn.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+
+      startBtn.addEventListener('click', countdownTime);
+
+      function countdownTime() {
+        timer = setInterval(() => {
+          startBtn.disabled = true;
+
+          const dateChoosenMs = new Date(dateChosen.value.replace(/-/g, '/')).getTime();
+          const now = new Date().getTime();
+          const timeLeft = dateChoosenMs - now;
+
+          const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+          d.innerHTML = days < 10 ? addLeadingZero(days) : days;
+          h.innerHTML = hours < 10 ? addLeadingZero(hours) : hours;
+          m.innerHTML = minutes < 10 ? addLeadingZero(minutes) : minutes;
+          s.innerHTML = seconds < 10 ? addLeadingZero(seconds) : seconds;
+
+          if (timeLeft < 1000) {
+            clearInterval(timer);
+            startBtn.disabled = false;
+          }
+        }, 1000);
+      }
+
+      function addLeadingZero(value) {
+        const stringValue = String(value);
+        return stringValue.padStart(2, '0');
+      }
+
+      function convertMs(ms) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const days = Math.floor(ms / day);
+        const hours = Math.floor((ms % day) / hour);
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+        return { days, hours, minutes, seconds };
+      }
+    }
   },
 };
 
-startButton.addEventListener('click', countdown);
-
-flatpickr(dataTime, options);
-
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-
-function countdown() {
-  const dateNow = new Date();
-  if (dateNow.getTime() >= selectedDates[0].getTime()) {
-    Notiflix.Notify.failure('Please choose a date in the future');
-    startButton.disabled = true;
-  } else {
-    startButton.disabled = false;
-    const timer = setInterval(() => {
-      const ms = selectedDates[0].getTime() - new Date().getTime();
-      const msRound = Math.floor(ms / 1000);
-      if (msRound === 0) {
-        clearInterval(timer);
-      }
-      convertMs(ms);
-    }, 1000);
-  }
-}
-
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  daysValue.textContent = addLeadingZero(Math.floor(ms / day));
-  hoursValue.textContent = addLeadingZero(Math.floor((ms % day) / hour));
-  minutesValue.textContent = addLeadingZero(
-    Math.floor(((ms % day) % hour) / minute)
-  );
-  secondsValue.textContent = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
-}
-
-
+flatpickr(dateChosen, options);
