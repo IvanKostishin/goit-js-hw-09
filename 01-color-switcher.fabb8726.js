fa8caaf75740cec61b013307c695bf2cd0d903e4
IvanKostishin <112808887+IvanKostishin@users.noreply.github.com>
2023-02-26 14:55:33 +0000

    Deploying to gh-pages from @ IvanKostishin/goit-js-hw-09@7baf7b0a91c2ca5d4419b58afddbfbf14ab4254e 🚀

--- 01-color-switcher.fabb8726.js
+++
@@ -1,2 +0,0 @@
-function t(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}const e={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};e.start.addEventListener("click",(function(){document.body.style.background=t(),e.start.disabled=!0,e.stop.disabled=!1,intervalColor=setInterval((()=>{document.body.style.background=t()}),1e3)})),e.stop.addEventListener("click",(function(){e.stop.disabled=!0,e.start.disabled=!1,clearInterval(intervalColor)})),e.start.disabled=!1;
-//# sourceMappingURL=01-color-switcher.fabb8726.js.map
